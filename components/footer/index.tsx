08ea8a2366b77c413ab709f3b4504690fdb0b09c
xyntechx <xyntechx@gmail.com>
2022-05-31 08:08:06 +0800

    ✨ New links + better mobile design

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,44 +5,115 @@ import styles from "./Footer.module.css";
 
 const Footer: NextPage = () => {
     return (
-        <footer className={styles.footer}>
-            <div className={styles.footeritemrow}>
-                <Image
-                    src="/logo.png"
-                    alt="NexLiber Logo"
-                    width={80}
-                    height={80}
-                />
-                <div className={styles.subfooteritem}>
-                    <p>MIT License</p>
-                    <br />
-                    <p>Copyright (c) Nyx Audrey Angelo Iskandar</p>
+        <>
+            {/* For Desktop */}
+            <footer className={styles.footer}>
+                <div className={styles.footeritemrow}>
+                    <Image
+                        src="/logo.png"
+                        alt="NexLiber Logo"
+                        width={80}
+                        height={80}
+                    />
+                    <div className={styles.subfooteritem}>
+                        <p>MIT License</p>
+                        <br />
+                        <p>Copyright (c) Nyx Audrey Angelo Iskandar</p>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.footeritemcol}>
-                <Link href="/faq">
-                    <a className={styles.footerlink}>FAQ</a>
-                </Link>
-                <br />
-                <Link href="https://www.buymeacoffee.com/xyntechx">
-                    <a className={styles.footerlink} target="_blank">
-                        Sponsor
-                    </a>
-                </Link>
-                <br />
-                <Link href="https://discord.gg/CvZGEjyzbR">
-                    <a className={styles.footerlink} target="_blank">
-                        Discord
-                    </a>
-                </Link>
-                <br />
-                <Link href="https://github.com/xyntechx/NexLiber">
-                    <a className={styles.footerlink} target="_blank">
-                        GitHub
-                    </a>
-                </Link>
-            </div>
-        </footer>
+                <div className={styles.footeritemcol}>
+                    <Link href="/faq">
+                        <a className={styles.footerlink}>FAQ</a>
+                    </Link>
+                    <br />
+                    <Link href="https://www.buymeacoffee.com/xyntechx">
+                        <a className={styles.footerlink} target="_blank">
+                            Support
+                        </a>
+                    </Link>
+                    <br />
+                    <Link href="https://github.com/xyntechx/NexLiber">
+                        <a className={styles.footerlink} target="_blank">
+                            Contribute
+                        </a>
+                    </Link>
+                </div>
+                <div className={styles.footeritemcol}>
+                    <Link href="https://discord.gg/CvZGEjyzbR">
+                        <a className={styles.footerlink} target="_blank">
+                            Discord
+                        </a>
+                    </Link>
+                    <br />
+                    <Link href="https://www.instagram.com/nexliber/">
+                        <a className={styles.footerlink} target="_blank">
+                            Instagram
+                        </a>
+                    </Link>
+                    <br />
+                    <Link href="https://www.linkedin.com/company/nexliber/">
+                        <a className={styles.footerlink} target="_blank">
+                            LinkedIn
+                        </a>
+                    </Link>
+                </div>
+            </footer>
+
+            {/* For Mobile */}
+            <footer className={styles.footermobile}>
+                <div className={styles.subfooter}>
+                    <div className={styles.footeritemcol}>
+                        <Link href="/faq">
+                            <a className={styles.footerlink}>FAQ</a>
+                        </Link>
+                        <br />
+                        <Link href="https://www.buymeacoffee.com/xyntechx">
+                            <a className={styles.footerlink} target="_blank">
+                                Support
+                            </a>
+                        </Link>
+                        <br />
+                        <Link href="https://github.com/xyntechx/NexLiber">
+                            <a className={styles.footerlink} target="_blank">
+                                Contribute
+                            </a>
+                        </Link>
+                    </div>
+                    <div className={styles.footeritemcol}>
+                        <Link href="https://discord.gg/CvZGEjyzbR">
+                            <a className={styles.footerlink} target="_blank">
+                                Discord
+                            </a>
+                        </Link>
+                        <br />
+                        <Link href="https://www.instagram.com/nexliber/">
+                            <a className={styles.footerlink} target="_blank">
+                                Instagram
+                            </a>
+                        </Link>
+                        <br />
+                        <Link href="https://www.linkedin.com/company/nexliber/">
+                            <a className={styles.footerlink} target="_blank">
+                                LinkedIn
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className={styles.footeritemrow}>
+                    <Image
+                        src="/logo.png"
+                        alt="NexLiber Logo"
+                        width={50}
+                        height={50}
+                    />
+                    <div className={styles.subfooteritem}>
+                        <p>MIT License</p>
+                        <br />
+                        <p>Copyright (c) Nyx Audrey Angelo Iskandar</p>
+                    </div>
+                </div>
+            </footer>
+        </>
     );
 };
 
