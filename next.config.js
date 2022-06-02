87c9e1059d8edbe4720749e35f662580b8810be0
xyntechx <xyntechx@gmail.com>
2022-06-02 16:10:25 +0800

    📇 Add domain for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ["raw.githubusercontent.com"],
+    },
+};
