f25b2dea0cf56836f65ef6a255bebd860f17af9a
xyntechx <xyntechx@gmail.com>
2022-09-15 12:58:28 +0800

    ✨ Use new structure of projects.json

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Head from "next/head";
 import Footer from "../components/footer";
 import Project from "../components/project";
@@ -7,6 +8,15 @@ import styles from "../styles/Library.module.css";
 import PROJECTS from "../public/projects.json";
 
 const Library: NextPage = () => {
+    const [projects, _] = useState<{
+        [key: string]: {
+            description: string;
+            field: string;
+            difficulty: string;
+            creator: string;
+        };
+    }>(PROJECTS);
+
     return (
         <div>
             <Head>
@@ -29,8 +39,12 @@ const Library: NextPage = () => {
                 <h1 className={styles.title}>Library</h1>
                 <br />
                 <section className={styles.content}>
-                    {PROJECTS.map((project) => (
-                        <Project key={project.title} {...project} />
+                    {Object.keys(projects).map((project_title) => (
+                        <Project
+                            key={project_title}
+                            title={project_title}
+                            {...projects[project_title]}
+                        />
                     ))}
                 </section>
                 <Footer />
