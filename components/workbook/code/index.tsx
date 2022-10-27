94ae5823d229fa980683ffab56fc55e261ae1029
xyntechx <xyntechx@gmail.com>
2022-10-27 14:23:57 +0800

    🔥 Revert changes

--- components/workbook/code/index.tsx
+++ components/workbook/code/index.tsx
@@ -1,22 +1,10 @@
-import Head from "next/head";
 import styles from "./Code.module.css";
 
 const Code = ({ code }: { code: string }) => {
     return (
-        <>
-            <Head>
-                <script
-                    async
-                    src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"
-                />
-                <script defer>hljs.highlightAll();</script>
-            </Head>
-            <div className={styles.container}>
-                <pre>
-                    <code style={{ borderRadius: "0.5rem" }}>{code}</code>
-                </pre>
-            </div>
-        </>
+        <div className={styles.container}>
+            <code className={styles.code}>{code}</code>
+        </div>
     );
 };
 
