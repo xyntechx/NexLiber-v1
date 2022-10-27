2af91c7a8a1007a2b43694a68299911f018aa056
xyntechx <xyntechx@gmail.com>
2022-10-27 14:08:23 +0800

    ✨ Syntax highlighting for code blocks

--- components/workbook/code/index.tsx
+++ components/workbook/code/index.tsx
@@ -1,10 +1,22 @@
+import Head from "next/head";
 import styles from "./Code.module.css";
 
 const Code = ({ code }: { code: string }) => {
     return (
-        <div className={styles.container}>
-            <code className={styles.code}>{code}</code>
-        </div>
+        <>
+            <Head>
+                <script
+                    async
+                    src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"
+                />
+                <script defer>hljs.highlightAll();</script>
+            </Head>
+            <div className={styles.container}>
+                <pre>
+                    <code style={{ borderRadius: "0.5rem" }}>{code}</code>
+                </pre>
+            </div>
+        </>
     );
 };
 
