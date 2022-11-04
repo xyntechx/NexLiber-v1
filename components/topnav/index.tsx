f56dc9bf6c90faca2a6671bde4bfce3f37f0e903
xyntechx <xyntechx@gmail.com>
2022-11-04 10:24:59 +0800

    ✨ Improve topnav

--- components/topnav/index.tsx
+++ components/topnav/index.tsx
@@ -43,7 +43,7 @@ const Topnav: NextPage = () => {
             <Link href="/">
                 <a>
                     <Image
-                        src={`/${activeTheme}/nexliber-bg.svg`}
+                        src={`/${activeTheme}/nexliber.svg`}
                         alt="NexLiber Logo"
                         width={80}
                         height={80}
@@ -51,33 +51,17 @@ const Topnav: NextPage = () => {
                 </a>
             </Link>
             <div className={styles.navlinks}>
-                <Link href="https://github.com/xyntechx/NexLiber">
-                    <a target="_blank" className={styles.imglink}>
-                        <Image
-                            src={`/${activeTheme}/github.png`}
-                            width={40}
-                            height={40}
-                            alt="NexLiber GitHub Repo"
-                        />
-                    </a>
-                </Link>
-                <Link href="/community">
-                    <a target="_blank" className={styles.imglink}>
-                        <Image
-                            src={`/${activeTheme}/discord.svg`}
-                            width={40}
-                            height={40}
-                            alt="NexLiber Discord Community"
-                        />
-                    </a>
-                </Link>
                 <Link href="/faq">
                     <a className={styles.link}>FAQ</a>
+                </Link>
+                <Link href="https://www.buymeacoffee.com/xyntechx">
+                    <a target="_blank" className={styles.link}>
+                        Sponsor
+                    </a>
                 </Link>
                 <Link href="/library">
                     <a className={styles.speciallink}>Library</a>
                 </Link>
-
                 <Toggle />
             </div>
         </header>
