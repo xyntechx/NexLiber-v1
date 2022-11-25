87854f37d95444df4d30a9d217f8120ae9c5db10
xyntechx <xyntechx@gmail.com>
2022-11-26 00:33:15 +0800

    🔨 Refactor code

--- pages/community.tsx
+++ pages/community.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Link from "next/dist/client/link";
+import Link from "next/link";
 
 const Community: NextPage = () => {
     return (
