7f7ae030bd8ced48915114ff02671a845fda9e74
xyntechx <xyntechx@gmail.com>
2023-02-19 10:03:18 +0800

    ✨ Update popup message

--- components/popup/index.tsx
+++ components/popup/index.tsx
@@ -17,47 +17,19 @@ const Popup = () => {
                     >
                         X
                     </button>
-                    <h1 className={styles.title}>NexLiber is growing up!</h1>
+                    <h1 className={styles.title}>
+                        NexLiber 2.0 is Coming Very Soon
+                    </h1>
                     <p className={styles.text}>
-                        Hi there! Thanks for stopping by!
-                    </p>
-                    <p className={styles.text}>
-                        What you&apos;re seeing here is NexLiber 1.0.0. NexLiber
-                        2.0.0 is currently under development.
-                    </p>
-                    <p className={styles.text}>
-                        I would appreciate your support as I develop NexLiber
-                        2.0.0 which is to be launched in early 2023. If
-                        you&apos;re feeling generous, you can{" "}
-                        <Link href="https://www.buymeacoffee.com/xyntechx">
+                        NexLiber 2.0 is releasing publicly on 1 March 2023! Join
+                        our{" "}
+                        <Link href="/community">
                             <a target="_blank" className={styles.link}>
-                                Buy Me a Coffee
-                            </a>
-                        </Link>
-                        ! If you&apos;d like to suggest or build new features
-                        for 2.0.0, visit NexLiber&apos;s{" "}
-                        <Link href="https://github.com/xyntechx/NexLiber/issues">
-                            <a target="_blank" className={styles.link}>
-                                GitHub
+                                Discord
                             </a>
                         </Link>{" "}
-                        repo. Visit{" "}
-                        <Link href="https://nexliber-2.vercel.app/">
-                            <a target="_blank" className={styles.link}>
-                                this
-                            </a>
-                        </Link>{" "}
-                        website to follow the progress of NexLiber 2.0.0.
-                    </p>
-                    <p className={styles.text}>
-                        Of course, this doesn&apos;t mean that you can&apos;t
-                        continue exploring this site; feel free to close this
-                        popup and continue learning with NexLiber! In fact, head
-                        on over to the{" "}
-                        <Link href="/library">
-                            <a className={styles.link}>Library</a>
-                        </Link>{" "}
-                        now!
+                        now to get an exclusive early access to NexLiber 2.0 on
+                        20 February!
                     </p>
                     <p className={styles.text}>Appreciate ya!</p>
                     <sub className={styles.author}>
